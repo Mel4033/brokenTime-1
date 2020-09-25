0afcba0111ff3d252c4cf4cbbf78e27e64af37c8
Axel <axel.baudain@gmail.com>
2020-09-25 14:49:16 +0200

    front-feature Firstlook on home page done

--- front/src/components/Home/index.js
+++ front/src/components/Home/index.js
@@ -2,89 +2,14 @@ import React from 'react';
 import './style.scss';
 
 const Home = () => (
-  <div className="home"> Home
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
-    <div className="fakeElement"> Element </div>
+  <div className="home">
+    {/* Encapsulation des éléments de première vue avec la div suivante */}
+    <div className="home__firstlook">
+      <icon className="home__item far fa-clock fa-8x" />
+      <h1 className="home__item home__title">Broken Time</h1>
+      <h2 className="home__item home__subtitle">Le site de fictions interactives.</h2>
+      <a className="home__item" href="#" alt="Autoscroll"><icon className="fas fa-angle-double-down fa-5x" /></a>
+    </div>
   </div>
 );
 
