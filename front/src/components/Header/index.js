e9e32b1f2fefb039c2f398d6a1cd92f92fd8755a
Remi Morel <mrl.remi@gmail.com>
2020-09-25 14:21:26 +0200

    front-feature add burger menu

--- front/src/components/Header/index.js
+++ front/src/components/Header/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './style.scss';
 
 const Header = () => (
-  <div className="header">Je suis le header</div>
+  <div className="header">
+    <button className="header__button">
+      <icon class="header__icon fas fa-bars fa-2x" />
+    </button>
+  </div>
 );
 
 export default Header;
