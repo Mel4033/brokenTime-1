8f70be3fac755a4efda895133de9f8f5ef342a23
Axel <axel.baudain@gmail.com>
2020-09-28 14:28:48 +0200

    front-bugfix Header had a strange behavior at 600px width

--- front/src/components/Header/Menu/index.js
+++ front/src/components/Header/Menu/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import './style.scss';
 
 const Menu = () => (
   <div className="menu">
-    <button type="button" className="header__button">
-      <icon className="header__icon fas fa-bars fa-2x" />
-    </button>
+    <div className="menu__burger">
+      <button type="button" className="burger__button">
+        <icon className="burger__icon fas fa-bars fa-2x" />
+      </button>
+    </div>
+    <div className="menu__desktop">
+      <div className="menu__links">
+        <a className="link" href="#" alt="none">Accueil</a>
+        <a className="link" href="#" alt="none">Bibliothèque</a>
+        <a className="link" href="#" alt="none">Mon profil</a>
+      </div>
+      <button type="button" className="menu__item menu__disconnect">Déconnexion</button>
+    </div>
   </div>
 );
 
