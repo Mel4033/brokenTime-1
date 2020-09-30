8b52230a5384176e5b51b24d621824cb202f9462
Axel <axel.baudain@gmail.com>
2020-09-30 13:27:50 +0200

    front-feature Login form structure done

--- front/src/components/Header/LoginForm/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const LoginForm = () => (
-  <div className="login__form">
-    <button alt="none" type="button" className="menu__item menu__disconnect"><i className="fas fa-power-off" /></button>
-  </div>
-);
-
-export default LoginForm;
--- front/src/components/Header/index.js
+++ front/src/components/Header/index.js
@@ -3,13 +3,13 @@ import './style-mobile.scss';
 import './style-desktop.scss';
 import BurgerMenu from './BurgerMenu';
 import DesktopMenu from './DesktopMenu';
-import LoginForm from './LoginForm';
+import UserForm from './UserForm';
 
 const Header = () => (
   <div className="header">
     <DesktopMenu />
     <BurgerMenu />
-    <LoginForm />
+    <UserForm />
   </div>
 );
 
