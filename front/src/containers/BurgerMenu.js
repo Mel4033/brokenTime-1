d8dcff74157b84ea1818a604fc8220788439f1c5
unknown <mrl.remi@gmail.com>
2020-10-01 14:44:47 +0200

    front feature burgerMenu WIP

--- front/src/containers/BurgerMenu.js
+++ front/src/containers/BurgerMenu.js
@@ -2,6 +2,8 @@ import { connect } from 'react-redux';
 import BurgerMenu from '../components/Header/BurgerMenu';
 import { inputChange } from '../actions/burgerMenu';
 
+const mapState = null;
 
+const mapDispatch = null;
 
 export default connect(mapState, mapDispatch)(BurgerMenu);
