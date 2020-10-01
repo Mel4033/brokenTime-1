ef1be2b118a529100cb0b7f7bd6a03d3cc80f0d0
Vincent A <accarias.vincent@gmail.com>
2020-10-01 16:31:43 +0200

    back-bugfix-user-done

--- back/BackBrokenTime/src/Controller/API/UserController.php
+++ back/BackBrokenTime/src/Controller/API/UserController.php
@@ -5,7 +5,10 @@ namespace App\Controller\API;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class UserController extends AbstractController
 {
@@ -37,6 +40,43 @@ class UserController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/user/new", name="user_new", methods={"POST"})
+     */
+    public function new(Request $request, SerializerInterface $serializer, ValidatorInterface $validator)
+    {
+        $dataJSON = $request->getContent();
+
+        $user = $serializer->deserialize($dataJSON, User::class, 'json');
+
+        $errors = $validator->validate($user);
+        $totalErrors = count($errors);
+        $success = false;
+        $message = '';
+
+        if ($totalErrors > 0) {
+            // Si on a des erreurs...alors on ne fait pas de sauvegarde...
+            // Et on prévient l'utilisateur
+            $message = "Il y a {$totalErrors} erreur(s) dans votre requete.";
+        } else {
+            // Pas d'erreur (à priori)
+            $success = true;
+            $message = "L'utilisateur a bien créé";
+
+            // ...on sauvegarde l'utilisateur en BDD
+            // $em = $this->getDoctrine()->getManager();
+            // $em->persist($user);
+            // $em->flush();
+        }
+
+        // On retourne un message pour dire que tout s'est bien passé...
+        return $this->json([
+            'success' => $success,
+            'message' => $message,
+            'errors' => $errors
+        ]);
+
+    }
 
     /**
      * @Route("/user/{slug}/success", name="user_success")
