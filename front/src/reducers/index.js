f995f5a4eef9c78ffb824f28486a8023cd2523a4
Axel <axel.baudain@gmail.com>
2020-10-02 13:41:39 +0200

    front-bugfix Bugfix on burger menu

--- front/src/reducers/index.js
+++ front/src/reducers/index.js
@@ -3,12 +3,10 @@ import counter from './counter';
 import cards from './cardData';
 import fiction from './fiction';
 import user from './user';
-import burgerMenu from './burgerMenu';
 
 export default combineReducers({
   counter,
   cards,
   fiction,
   user,
-  burgerMenu,
 });
