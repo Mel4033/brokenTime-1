0b4d85edb4f3936ca4f6ae3a740e00c7869435ff
Vincent A <accarias.vincent@gmail.com>
2020-10-04 13:09:11 +0200

    back-feature-Test-Cors-WIP

--- back/BackBrokenTime/src/Entity/User.php
+++ back/BackBrokenTime/src/Entity/User.php
@@ -27,7 +27,7 @@ class User implements UserInterface
     private $email;
 
     /**
-     * @ORM\Column(type="json")
+     * @ORM\Column(type="json", nullable=true)
      */
     private $roles = [];
 
