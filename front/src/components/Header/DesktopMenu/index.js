45ad74e2021e1fccdab54782e4d3a2293f6afe7f
unknown <mrl.remi@gmail.com>
2020-10-06 15:50:12 +0200

    front-feature-team-page adding road to the TeamPage

--- front/src/components/Header/DesktopMenu/index.js
+++ front/src/components/Header/DesktopMenu/index.js
@@ -7,6 +7,7 @@ const DesktopMenu = () => (
       <NavLink exact to="/" className="link" href="#" alt="none">Accueil</NavLink>
       <NavLink exact to="/library" className="link" href="#" alt="none">Bibliothèque</NavLink>
       <NavLink exact to="/profil" className="link" href="#" alt="none">Mon profil</NavLink>
+      <NavLink exact to="/TeamPage" className="link" href="#" alt="none">L'équipe</NavLink>
     </div>
   </div>
 );
