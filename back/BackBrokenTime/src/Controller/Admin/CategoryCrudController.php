616d879fa36a88d6320e363fd49aa4e033e0fe26
Mel4033 <melinda4033@gmail.com>
2020-10-07 14:55:24 +0200

    back-uploadPictureWIP

--- back/BackBrokenTime/src/Controller/Admin/CategoryCrudController.php
+++ back/BackBrokenTime/src/Controller/Admin/CategoryCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Category;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class CategoryCrudController extends AbstractCrudController
@@ -18,6 +19,7 @@ class CategoryCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
+            IdField::new('id', 'id'),
             TextField::new('name', 'Nom'),
             DateField::new('created_at', 'Date de création'),
         ];
