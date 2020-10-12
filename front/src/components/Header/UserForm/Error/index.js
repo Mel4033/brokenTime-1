f3b7fcb927848ce01a4c96384146ae6c38bd95d9
Axel <axel.baudain@gmail.com>
2020-10-12 10:33:13 +0200

    front-bugfix after a login or register success, the form now closes itself properly

--- front/src/components/Header/UserForm/Error/index.js
+++ front/src/components/Header/UserForm/Error/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Error = ({ receivedError = 'erreur à déterminer' }) => (
+const Error = ({ receivedError = 'Une erreur est survenue.' }) => (
   <div className="error">{receivedError}</div>
 );
 
