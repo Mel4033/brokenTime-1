8077305f3746c835e48e70520e85ad6726eb5b4d
Axel <axel.baudain@gmail.com>
2020-10-13 09:48:27 +0200

    front-bugfix Changing user's default image

--- front/src/components/Library/Card/index.js
+++ front/src/components/Library/Card/index.js
@@ -5,7 +5,6 @@ import './style-mobile.scss';
 // Je vais ici créer et dynamiser mes cards, que je vais par la suite appeller dans ma library j'importe mon style scss desktop et 
 // mobile, que j'ai aussi dans mon index de card
 const Card = ({ title, resume, locked, thumbnail }) => (
-
     <div className="cardUnit">
       <div style={{ backgroundImage: `url(${thumbnail})`, backgroundSize: 'cover', backgroundPosition: 'center' }} className="thumbnail">
       <h4 className="fictionTitle">{title}</h4>  
@@ -13,7 +12,6 @@ const Card = ({ title, resume, locked, thumbnail }) => (
           {resume}</p>
       </div>
     </div>
-
 );
 
 export default Card;
--- front/src/containers/UserReadCard.js
+++ front/src/containers/UserReadCard.js
@@ -1,8 +1,9 @@
 import { connect } from 'react-redux';
 import UserReadCard from '../components/Profil/UserCard/UserReadCard';
-import defaultImage from '../assets/images/usericon.png';
 
 import { switchProfilDisplay } from '../actions/profilCard';
+
+const defaultImage = 'https://media.discordapp.net/attachments/364094342953959424/765477171555860490/usericon.png';
 
 // Application d'une image de profil par défaut dans le cas
 // où l'utilisateur possède une image de profil égale à null.
