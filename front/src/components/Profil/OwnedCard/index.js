8f05ad064cd563ef2f21479e63a2656271b6e186
Axel <axel.baudain@gmail.com>
2020-10-13 14:27:45 +0200

    front-bugfix Buttons are now aligned properly on profile interface

--- front/src/components/Profil/OwnedCard/index.js
+++ front/src/components/Profil/OwnedCard/index.js
@@ -7,18 +7,20 @@ const OwnedCard = ({ id, title, resume, image }) => {
   const isAvailable = id === 1;
   const launchMessage = 'Lancer l\'aventure';
   return (
-    <div className="fiction__card" style={{ backgroundImage: `url(${image})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-      <div className="fiction__informations">
-        <h4 className="fiction__title">{title}</h4>
-        <p className="fiction__resume">{resume}</p>
-        {isAvailable && (
-          <Link to="/fiction">
-            <button type="button" className="profile-fiction__play-button profile-fiction__play-button-logged">
-              {launchMessage} <i className="fas fa-caret-right icon__button" />
-            </button>
-          </Link>
-        )}
-        { !isAvailable && <button type="button" className="profile-fiction__play-button unavailable">Indisponible...</button> }
+    <div className="profile-fiction__card" style={{ backgroundImage: `url(${image})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+      <div className="profile-fiction__informations">
+        <h4 className="profile-fiction__title">{title}</h4>
+        <div className="profile-fiction__resume--container">
+          <p className="profile-fiction__resume">{resume}</p>
+          {isAvailable && (
+            <Link to="/fiction">
+              <button type="button" className="profile-fiction__play-button profile-fiction__play-button-logged">
+                {launchMessage} <i className="fas fa-caret-right icon__button" />
+              </button>
+            </Link>
+          )}
+          {!isAvailable && <button type="button" className="profile-fiction__play-button unavailable">Indisponible...</button>}
+        </div>
       </div>
     </div>
   );
